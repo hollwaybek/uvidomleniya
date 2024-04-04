9c68c870a19a315f86827246f1347c319ea61f0d
hollwaybek <hollwayuzb@gmail.com>
2024-04-04 15:17:57 +0500

    done

---
+++ main.js
@@ -0,0 +1,6 @@
+let ism = prompt('ism')
+let fam = prompt('fam')
+let yosh = prompt('yosh')
+let adres = prompt('adress')
+let fan = prompt('fan')
+console.log(`Foydalanuvchi ismi: ${ism} \nFoydalanuvchi familiasi: ${fam} \nFoydalanuvchi yoshi: ${yosh} \nFoydalanuvchi manzili: ${adres} \nFOydalanuvchi yoqtirgan fan: ${fan} \n`);
